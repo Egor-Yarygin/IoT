5f1e06e92beb09ee2ecddd7a970dc684fa97b71d
Egor-Yarygin <108565142+Egor-Yarygin@users.noreply.github.com>
2023-10-23 21:57:20 +0800

    Add files via upload

---
+++ ESP_Lamp/Config.h
@@ -0,0 +1,13 @@
+String AP_NAME = "Eto_Moyo"; // AP WIFI access point
+String AP_PASSWORD = "12345678";
+
+// CLI WIFI client
+const char* CLI_SSID = "OPPO_A96"; // turn phone AP 5 GHz -> 2.4 GHz
+const char* CLI_PASS = "00000000";
+
+int WEB_SERVER_PORT = 80;
+
+const char* mqtt_broker = "broker.emqx.io";
+const int mqtt_port = 1883;
+
+bool AP_mode = true;
