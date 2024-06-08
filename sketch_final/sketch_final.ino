82f09e00df7ce77ed7daa443ccf87da1e6b83927
Daniil Botvenko <134534485+Kadyrovecdsgn@users.noreply.github.com>
2024-06-08 12:07:48 +0800

    main sketch

---
+++ sketch_final/sketch_final.ino
@@ -0,0 +1,51 @@
+#include <ESP8266WiFi.h>
+#include <Servo.h>
+#include <SPI.h>
+#include <MFRC522.h>
+#include <ArduinoJson.h>
+
+#define LOCK_TIMEOUT 3000
+#define SERVO_PIN D2
+#define BUZZER_PIN D3
+#define RED_LED_PIN D14
+#define RST_PIN D9
+#define CS_PIN D10
+#define BTN_PIN D8
+#define DOOR_PIN A0
+
+#include "Config.h"
+#include "Lock_control.h"
+#include "Server.h"
+#include "WIFI.h"
+
+bool locked = true;
+uint32_t lockTimeout;
+MFRC522 rfid(CS_PIN, RST_PIN);
+Servo doorServo;
+
+const char* serverName = "http://192.168.0.100:8000/uid";
+const char* ueid = "r32r32r32r32"; // Добавляем ueid здесь
+
+void setup() {
+  pinMode(BTN_PIN, INPUT_PULLUP);
+  pinMode(DOOR_PIN, INPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(RED_LED_PIN, OUTPUT);
+  SPI.begin();
+  rfid.PCD_Init();
+  Serial.begin(9600);
+  setupWiFiAP();
+  server_init();
+  lock();
+}
+
+void loop() {
+  server.handleClient();
+  
+  state_led();
+  handleButtonPress();
+  handleDoorSensor();
+  handleLockTimeout();
+  handleRFID();
+  rebootRFID();
+}
